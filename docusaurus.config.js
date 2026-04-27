ef98d9ccc781b10916855233c94fa9cc149f3aa0
Bruno Bremert <brunog.bremert@gmail.com>
2026-04-27 15:22:08 +0100

    Configuração para usar o GitHub Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://BrunoBremert.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/dokku/',
@@ -28,7 +28,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'BrunoBremert', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'dokku', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
